3a95a60618662b5003b19d3aa35663266d9884ad
Facundo Martin Cabrera <cabre94@hotmail.com>
2025-03-10 17:05:37 -0300

    update getPossibleMoves pawn #29

--- include/Pieces/Pawn.h
+++ include/Pieces/Pawn.h
@@ -14,12 +14,25 @@ public:
                 (player_id_ == WHITE) ? PAWN_WHITE_SYMBOL : PAWN_BLACK_SYMBOL, pos_) {}
     ~Pawn() {}
 
-    std::set<Position> getPossibleMoves(const Board *board) const override {
-        (void) board;
-        std::set<Position> validMoves;
-        // validMoves = board->getValidMoves(from, PAWN); // TODO
+    std::set<Position> getPossibleMoves(const Board *board) override {
+        // (void) board;
+        // std::set<Position> validMoves;
+        // // validMoves = board->getValidMoves(from, PAWN); // TODO
+        // return validMoves;
+        std::vector<int16_t> forward_dir;
+        if (player_id == WHITE)
+            forward_dir = {1, 0};
+        else
+            forward_dir = {-1, 0};
+
+        std::set<Position> validMoves =
+            board->getFordwardMoves(pos, player_id, forward_dir, first_move);
+
+        first_move = false;
         return validMoves;
     }
+
+    bool first_move = true;
 };
 
 } // namespace chess
