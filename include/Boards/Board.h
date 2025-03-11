172e5879cbd9ecbb5d0cb7e47cc6a700ad8bd053
Facundo Martin Cabrera <cabre94@hotmail.com>
2025-03-11 15:04:40 -0300

    Clean code

--- include/Boards/Board.h
+++ include/Boards/Board.h
@@ -25,7 +25,7 @@ public:
     virtual void printBoard() const = 0;
     virtual void placePieces(const std::vector<Piece *> &v_pieces) = 0;
 
-    // Aca deberian poner los metodos que dan las posiciones validas
+    // TODO: move this to private?
     virtual std::set<Position> getDiagonalMoves(const Position &pos,
                                                 const PlayerID player_id) const = 0;
     virtual std::set<Position> getParallelMoves(const Position &pos,
@@ -43,7 +43,6 @@ public:
     virtual Piece *getPiece(uint32_t r, uint32_t c) = 0;
 
     // virtual bool makeMove(Position from, Position to) = 0;
-    // virtual void createPices(const char c = 'n') = 0;
 };
 
 } // namespace chess
