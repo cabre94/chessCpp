d0ec341dbc3eb2937fc30d25c5db9ebc015be7cb
Facundo Martin Cabrera <cabre94@hotmail.com>
2025-03-12 12:07:32 -0300

    Add aux functions to remove pieces froms vector and free memory #42

--- include/ChessGame.h
+++ include/ChessGame.h
@@ -25,6 +25,9 @@ protected:
     void createPieces();
 
     void freePieces(std::vector<Piece *> &pieces);
+    void findFreePiece(Piece *piece);
+
+    bool removeFromVector(std::vector<Piece *> &pieces, Piece *target);
 
     Board *board;
 
--- src/ChessGame.cpp
+++ src/ChessGame.cpp
@@ -1,4 +1,5 @@
-#include "ChessGame.h"
+#include <algorithm>
+
 #include "Boards/Board.h"
 #include "Boards/ChessBoard.h"
 #include "Pieces/Bishop.h"
@@ -10,6 +11,8 @@
 #include "Pieces/Queen.h"
 #include "Pieces/Rook.h"
 #include "Positions/Position.h"
+
+#include "ChessGame.h"
 
 namespace chess {
 
@@ -80,6 +83,25 @@ void ChessGame::freePieces(std::vector<Piece *> &pieces) {
     for (Piece *p : pieces)
         delete p;
     pieces.clear();
+}
+
+void ChessGame::findFreePiece(Piece *piece) {
+    // Try to remove from w_pieces or b_pieces
+    bool found = removeFromVector(w_pieces, piece) || removeFromVector(b_pieces, piece);
+
+    assert(found && "Error: piece not found in any of the vectors.");
+
+    delete piece;
+}
+
+bool ChessGame::removeFromVector(std::vector<Piece *> &pieces, Piece *target) {
+    auto it = std::find(pieces.begin(), pieces.end(), target);
+    if (it != pieces.end()) {
+        std::iter_swap(it, pieces.end() - 1);
+        pieces.pop_back();
+        return true;
+    }
+    return false;
 }
 
 } // namespace chess
