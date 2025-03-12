c5ccd9380851b95bf872c5fe0cb47c0eb3c51c76
Facundo Martin Cabrera <cabre94@hotmail.com>
2025-03-12 10:42:51 -0300

    remove assert #42

--- include/Boards/ChessBoard.h
+++ include/Boards/ChessBoard.h
@@ -52,7 +52,7 @@ protected:
     void setPiece(const Position &pos, Piece *piece) {
         if (!validPos(pos))
             throw std::invalid_argument("Invalid position");
-        assert(pieces[pos[1]][pos[0]] == nullptr);
+        // assert(pieces[pos[1]][pos[0]] == nullptr);
         pieces[pos[1]][pos[0]] = piece;
     }
 
