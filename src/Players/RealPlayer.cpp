2d41afc5b78b8c4bf2707b4bdd58e75c99ae0f54
Facundo Martin Cabrera <cabre94@hotmail.com>
2025-03-14 11:37:36 -0300

    #52

--- src/Players/RealPlayer.cpp
+++ src/Players/RealPlayer.cpp
@@ -5,10 +5,15 @@ namespace chess {
 Position RealPlayer::askPosition() const {
     std::string input;
 
-    std::cout << "Select position: \n";
-    std::cin >> input;
+    while (true) {
+        try {
+            std::cout << "Select position: \n";
+            std::cin >> input;
 
-    return Position(input);
+            return Position(input);
+
+        } catch (const std::invalid_argument &e) { std::cerr << "Invalid position. Try again.\n"; }
+    }
 }
 
 } // namespace chess
